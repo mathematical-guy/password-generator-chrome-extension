fd64693aa3e593ba1fceccbd1794278ae14e8807
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-10-05 18:04:56 +0530

    Upper/Lower/Digits/Special character dynamic feat

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ const imgIcon = document.getElementById('clipboard-icon');
 const lengthParamEle = document.getElementById('lengthParam');
 const lengthParamDisplayEle = document.getElementById('lengthParamDisplay');
 
+const includeUpperCaseEle = document.getElementById('uppercase');
+const includeLowerCaseEle = document.getElementById('lowercase');
+const includeNumberEle = document.getElementById('numbers');
+const includeSpecialCharEle = document.getElementById('special');
+
 
 // Icon Paths
 const copiedSuccessIcon = './copied-success.png';
 const clipBoardIcon = './copy-icon.png';
 
+includeLowerCaseEle.addEventListener('change', updatePasswordToDisplay);
+includeUpperCaseEle.addEventListener('change', updatePasswordToDisplay);
+includeNumberEle.addEventListener('change', updatePasswordToDisplay);
+includeSpecialCharEle.addEventListener('change', updatePasswordToDisplay);
+
 
 function changePasswordLength() {
-    lengthParamDisplayEle.innerText= lengthParamEle.value;
-
+    lengthParamDisplayEle.innerText = lengthParamEle.value;
     updatePasswordToDisplay();
 }
 
@@ -22,15 +31,30 @@ function updatePasswordToDisplay() {
     generatedPasswordEle.innerText = generateRandomString();
 }
 
-
 function generateRandomString() {
-    let length = lengthParamEle.value;
     const UPPER_CASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz';
     const DIGITS = '0123456789';
     const SPECIAL_CHARACTERS = '!@#$%^&*();[]{}-_`~,.';
 
-    const characters = UPPER_CASE + LOWER_CASE + DIGITS + SPECIAL_CHARACTERS;
+    let length = lengthParamEle.value;
+
+    let characters = '';
+
+    if (includeLowerCaseEle.checked) {
+        characters += LOWER_CASE;
+    }
+    if (includeUpperCaseEle.checked) {
+        characters += UPPER_CASE;
+    }
+    if (includeSpecialCharEle.checked) {
+        characters += SPECIAL_CHARACTERS;
+    }
+    if (includeNumberEle.checked) {
+        characters += DIGITS;
+    }
+
+    // const characters = UPPER_CASE + LOWER_CASE + DIGITS + SPECIAL_CHARACTERS;
     let result = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
